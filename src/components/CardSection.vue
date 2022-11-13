86736ffd9bfbd03f4839da7dda3719af793ce396
yosra <yosra.feili.tiara.studio>
2022-11-13 15:42:25 +0330

    add card component to home page

---
+++ src/components/CardSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-center space-x-4">
+    <div class="w-1/5 space-y-4">
+      <CardClub title="خرید امتیاز" type="prize"/>
+      <CardClub  sub-title="ماراتنی وجود ندارد" type="marathon"/>
+    </div>
+    <div class="w-1/5 space-y-4">
+      <CardClub title="پیش بینی بازی فوتبال" sub-title="بازی را پیش بینی کنید و جایزه بگیرید" type="footballCard"/>
+      <CardClub title="سوال وجواب" sub-title="به سوال ها جواب بده و جایزه ببر" type="qa"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardClub from '@/components/CardClub'
+export default {
+  name: 'CardSection',
+  components: { CardClub }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="">
+   <div class="mt-20">
+     <CardSection/>
+   </div>
+ </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import CardSection from '@/components/CardSection'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { CardSection }
 }
 </script>
