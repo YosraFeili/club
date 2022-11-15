49ef165441ff1292a274527390fe98c3be79c378
yosra <yosra.feili.tiara.studio>
2022-11-15 14:05:55 +0330

    add pop up

---
+++ src/components/PopUp.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="bg-popup">
+    <div class="icon-cancel">
+      <img src="../assets/icons/close.svg" @click="close">
+    </div>
+    <div class="card">
+      <div class="card-header">خرید پکیج</div>
+      <div class="card-body">
+        <div class="product-card" :class="{selected}">
+          <div class="">4000تومان</div>
+          <div class="product-row space-x-3">
+            <label for="product.id">پکیج 10 امتیازی</label>
+            <input id="product.id" type="radio" name="products" value="product" v-model="selected">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PopUp',
+  data () {
+    return {
+      selected: ''
+    }
+  },
+
+  methods: {
+    close () {
+      this.$emit('closePopUp')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bg-popup {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(42, 44, 65, 0.49);
+  overflow: hidden;
+  display: block;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  backdrop-filter: blur(5px);
+  z-index: 100;
+}
+
+.icon-cancel {
+  position: relative;
+  top: 50px;
+  left: 50px;
+  cursor: pointer;
+}
+
+.card {
+  position: relative;
+  top: 12.5%;
+  left: 25%;
+  background: #2A2C41FF;
+  width: 50%;
+  border-top-left-radius: 7px;
+  border-top-right-radius: 7px;
+}
+.card-header{
+  border-top-left-radius: 7px;
+  border-top-right-radius: 7px;
+  background: #1c1d32;
+  text-align: center;
+  color: white;
+  padding: 20px;
+  font-family: Yekan;
+  font-size: 18px;
+}
+.card-body{
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
+.product-card{
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid #FFFFFF33;
+  padding: 20px;
+  color: white;
+  font-family: Yekan;
+}
+
+/*input[type='radio'] + label {*/
+/*  display: inline-block;*/
+/*  cursor: pointer;*/
+/*  position: relative;*/
+/*  padding-right: 30px;*/
+/*  font-size: 13px;*/
+/*  color: white;*/
+/*}*/
+/*.selected {*/
+/*   border: 1px solid #fdbf50;*/
+/* }*/
+/*input[type='radio'] {*/
+/*  display: none !important;*/
+/*  *display: inline;*/
+/*}*/
+.product-row {
+  display: flex;
+  align-items: center;
+}
+
+</style>
--- src/components/cards/PurchasCard.vue
+++ src/components/cards/PurchasCard.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="">
-    <router-link :to="{name: 'purchase' }">
-      <div class="purchase">
+      <div class="purchase" @click="purchasePopUp=true">
         <div class="space-y-2">
           <div class="text-lg mt-6">خرید امتیاز</div>
         </div>
       </div>
-    </router-link>
+    <div class="">
+      <PopUp v-if="purchasePopUp" @closePopUp="close"/>
+    </div>
   </div>
 </template>
 
 <script>
+import PopUp from '@/components/PopUp'
 export default {
-  name: 'PurchaseCard'
+  name: 'PurchaseCard',
+  components: { PopUp },
+  data () {
+    return {
+      purchasePopUp: false
+    }
+  },
+  methods: {
+    close () {
+      this.purchasePopUp = false
+    }
+  }
 }
 </script>
 
