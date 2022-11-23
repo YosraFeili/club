a110f68ddb9e8b98b0183a913f8aa8c46f5f3e88
yosra <yosra.feili.tiara.studio>
2022-11-23 11:53:14 +0330

    fix responsive pop up

--- src/components/dashboard/WebDashboard.vue
+++ src/components/dashboard/WebDashboard.vue
@@ -12,7 +12,9 @@
       </div>
       <div class="web-dashboard-body">
         <hr>
-        <div class="text-white text-center p-5 cursor-pointer">داشبورد</div>
+        <div class="text-white text-center p-5 cursor-pointer">
+          <router-link :to="{name: 'dashboard'}">داشبورد</router-link>
+        </div>
         <hr>
         <div class="text-white text-center p-5 cursor-pointer">
           <router-link :to="{name: 'get-prize'}">دریافت جایزه</router-link>
